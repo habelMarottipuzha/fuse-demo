13ef65ee93179ff7ecbccf9ae4b3c9cc22339660
Aanandhu Vijayan <anandhuvijayank@gmail.com>
2024-07-21 17:12:56 +0530

    route hashing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -18,6 +18,7 @@ import { mockApiServices } from 'app/mock-api';
 import { firstValueFrom } from 'rxjs';
 import { TranslocoHttpLoader } from './core/transloco/transloco.http-loader';
 import { authHttpInterceptorFn, provideAuth0 } from '@auth0/auth0-angular';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 const isLocal = window.location.origin.includes('localhost');
 const config = {
@@ -75,6 +76,7 @@ export const appConfig: ApplicationConfig = {
             withPreloading(PreloadAllModules),
             withInMemoryScrolling({ scrollPositionRestoration: 'enabled' }),
         ),
+        { provide: LocationStrategy, useClass: HashLocationStrategy },
         // Material Date Adapter
         {
             provide: DateAdapter,
